747193dc481e4051e8fa7138cf94b24fe361e47b
val0101001 <105948771+val0101001@users.noreply.github.com>
2023-04-09 00:14:53 -0500

    Update main.cpp

--- tarea/main.cpp
+++ tarea/main.cpp
@@ -9,5 +9,9 @@
 
 // Puede validar su codigo con la siguiente funcion main
 int main(){
-
+    srand(time(0));
+    Matriz2D m1(3,4);
+    Matriz2D m2(4,3);
+    Matriz2D m3=m1*m2;
+    cout<<m1<<"\n"<<m2<<"\n"<<m3;
 }
